a2e9e4e46a43e84b2998c29e601669e64198ed91
pugmarx <naqi.rizvi@gmail.com>
2020-04-25 21:00:30 +0100

    fixed recurrence and response to joystick

--- en/resources/promodoro-timer.py
+++ en/resources/promodoro-timer.py
@@ -8,7 +8,11 @@ sense = SenseHat()
 sense.set_rotation(90)
 sense.low_light = True
 
+SLEEP_SEC = 23.4375
+#SLEEP_SEC = .5
 
+MAX_RANGE=64
+#MAX_RANGE=10
 
 G = [0, 255, 100]  # green
 R = [255, 100, 0] # red
@@ -18,31 +22,39 @@ secs = 1500
 #timer = [ G for i in range(secs)] + [ X for j in range(64 - secs) ]
 
 while True:
+  global i
+  sense.clear()
   timer = [X for j in range(64)]
 
   sense.set_pixels(timer)
 
-  global i
 
   # 1500 second timer
-  for i in range(0, 64):
+  for i in range(0, MAX_RANGE):
   
+    global flag
+    flag = False
     print("i = {}".format(i))  
-    #sleep(23.4375)
-    sleep(1)
+    sleep(SLEEP_SEC)
     timer[i] = R
     sense.set_pixels(timer)
     while True:
       for event in sense.stick.get_events():
           print("The joystick was {} {}".format(event.action, event.direction))
           if event.action == ACTION_RELEASED:
-              i = 0
+              flag = True
               sense.clear()
       break
-  if i < 64:
+   
+    
+    #print("flag {}".format(flag))
+    if flag == True:
+      i = 0
       break
+    #if i < 63:
+    #   break
 
-  for i in range(0, 64):
+  for i in range(0, 3):
     sense.clear()
     sleep(0.1)
     sense.set_pixels(timer)
